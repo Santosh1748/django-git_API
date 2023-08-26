91e3b72550c854672555019d4d6efe57b72ce90b
Santosh1748 <yadavsantosh5865@gmail.com>
2023-08-26 17:11:38 +0530

    commit:18

--- watchmate/myappone/api/serializers.py
+++ watchmate/myappone/api/serializers.py
@@ -7,7 +7,15 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField()
     active = serializers.BooleanField()
 
-    
+
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+    
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
