74ea3f9569cbd26f88cf8aab416cc9e9fcd8c57d
Santosh1748 <yadavsantosh5865@gmail.com>
2023-08-26 16:48:05 +0530

    commit:18

--- watchmate/myappone/models.py
+++ watchmate/myappone/models.py
@@ -4,10 +4,9 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=50)
-    Description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
-    
     
